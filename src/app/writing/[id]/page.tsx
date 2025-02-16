fbb4249781f627605fbd9e3182213b792df73e1b
SHIM-JINSEO <sjs011828@gmail.com>
2025-02-17 00:01:00 +0900

    feat: add share button and apply button on detail page

--- src/app/writing/[id]/page.tsx
+++ src/app/writing/[id]/page.tsx
@@ -2,11 +2,12 @@ import { Client } from "@notionhq/client";
 import { NotionToMarkdown } from "notion-to-md";
 
 import { unified } from "unified";
-
 import parse from "remark-parse";
 import stringify from "rehype-stringify";
 import toRehype from "remark-rehype";
 
+import Link from "next/link";
+import ShareButton from "@/app/components/ShareButton/ShareButton";
 const notionKey = process.env.NOTION_KEY;
 const notion = new Client({ auth: notionKey });
 const n2m = new NotionToMarkdown({
@@ -16,7 +17,7 @@ const n2m = new NotionToMarkdown({
 n2m.setCustomTransformer("embed", async (block) => {
   const { embed } = block as any;
   if (!embed?.url) return "";
-  return `<figure>
+  return `<figure style="margin: 20px;">
   <iframe src="${embed?.url}"></iframe>
   <figcaption>${await n2m.blockToMarkdown(embed?.caption)}</figcaption>
 </figure>`;
@@ -35,9 +36,19 @@ export default async function DetailPage(props) {
     .process(markdown);
   const htmlString = html.toString();
   return (
-    <div>
-      <div dangerouslySetInnerHTML={{__html: htmlString}}/>
+    <div className="flex flex-col items-center">
+      <div dangerouslySetInnerHTML={{ __html: htmlString }} />
+      <div className="flex flex-col items-end w-[900px] my-[50px]">
+        <ShareButton url={`https://www.notion.so/infoteam-rulrudino/${pageId}`} />
+      </div>
+      <h4>인포팀에서 함께 일하고 싶다면?</h4>
+      
+      <Link
+        href="/"
+        className="text-sm px-[12px] py-[8px] rounded-[5px] border-2 border-[#FF4500] text-[#FF4500]"
+      >
+        지원 바로가기
+      </Link>
     </div>
   );
 }
-
