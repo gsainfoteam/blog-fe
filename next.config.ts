673d91530b1ebf42e7b30a7a175e45ff57baed64
SHIM-JINSEO <sjs011828@gmail.com>
2025-02-21 17:03:26 +0900

    feat: redirect to /main

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['prod-files-secure.s3.us-west-2.amazonaws.com'],
+    domains: ["prod-files-secure.s3.us-west-2.amazonaws.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/main",
+        permanent: true, 
+      },
+    ];
   },
 };
 
