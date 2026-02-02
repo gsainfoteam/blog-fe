108af0eae5c46394ae660b27e8168d4039043e97
2paperstar <lee@paperst.ar>
2026-02-02 14:13:51 +0900

    feat: add article page

---
+++ app/article/[id]/[title]/notion-wrapper.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { ExtendedRecordMap } from "notion-types";
+import { NotionRenderer } from "react-notion-x";
+// core styles shared by all of react-notion-x (required)
+import "react-notion-x/src/styles.css";
+
+// used for code syntax highlighting (optional)
+import "prismjs/themes/prism-tomorrow.css";
+
+// used for rendering equations (optional)
+import "katex/dist/katex.min.css";
+
+import { Code } from "react-notion-x/build/third-party/code";
+import { Collection } from "react-notion-x/build/third-party/collection";
+import { Equation } from "react-notion-x/build/third-party/equation";
+import { Modal } from "react-notion-x/build/third-party/modal";
+import { Pdf } from "react-notion-x/build/third-party/pdf";
+
+export default function NotionWrapper({
+  recordMap,
+}: {
+  recordMap: ExtendedRecordMap;
+}) {
+  return (
+    <NotionRenderer
+      recordMap={recordMap}
+      components={{ Code, Collection, Equation, Modal, Pdf }}
+      disableHeader
+      fullPage={true}
+      darkMode={false}
+    />
+  );
+}
---
+++ app/article/[id]/[title]/share-button.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { FaLink } from "react-icons/fa";
+
+export default function ShareButton({ url }: { url: string }) {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopyLink = () => {
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000); // 2초 후 복사 완료 메시지 숨김
+      })
+      .catch((error) => {
+        console.error("Error copying text: ", error);
+      });
+  };
+
+  return (
+    <div className="flex items-center">
+      <FaLink />
+      <button className="" onClick={handleCopyLink}>
+        {copied ? "링크가 복사되었습니다!" : "공유하기"}
+      </button>
+    </div>
+  );
+}
