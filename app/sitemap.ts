92dec0331024bbea42ae481486a1b64514addf77
2paperstar <lee@paperst.ar>
2026-05-24 06:10:07 +0900

    feat: sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,27 @@
+import { getNotionData } from "@/utils/notion";
+import type { MetadataRoute } from "next";
+import { getTitle } from "./(main)/article-item";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const data = await getNotionData(null, null);
+  const result = data.map((page) => ({
+    id: page.id,
+    title: getTitle(page, true),
+    lastModified: page.last_edited_time,
+  }));
+  return [
+    {
+      url: "https://blog.gistory.me",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+    },
+    ...result.map(
+      (page) =>
+        ({
+          url: `https://blog.gistory.me/article/${page.id}/${page.title}`,
+          lastModified: page.lastModified,
+          changeFrequency: "weekly",
+        }) as const,
+    ),
+  ];
+}
