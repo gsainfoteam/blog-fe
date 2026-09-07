f28749d1b7e44f8ff021449c999c6cc5da55abce
2paperstar <lee@paperst.ar>
2026-09-07 12:29:59 +0900

    chore: tailwind prettier, eslint setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import tailwindcss from "eslint-plugin-tailwindcss";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,15 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  tailwindcss.configs['flat/recommended'] || tailwindcss.configs.recommended,
+  {
+    settings: {
+      react: { version: "19" },
+      tailwindcss: {
+        cssConfigPath: "./app/globals.css",
+      },
+    }
+  },
 ]);
 
 export default eslintConfig;
